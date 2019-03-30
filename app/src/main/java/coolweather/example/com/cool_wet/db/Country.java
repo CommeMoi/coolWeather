011cd9e42e2d9b73dc1c97fb88116cac4a3eeb96
yanyukun <yanyukun@aidigger.com>
2019-03-30 10:39:57 +0800

    First commit

---
+++ app/src/main/java/coolweather/example/com/cool_wet/db/Country.java
@@ -0,0 +1,43 @@
+package coolweather.example.com.cool_wet.db;
+
+import org.litepal.crud.DataSupport;
+
+public class Country extends DataSupport {
+    private int id;
+    private String countryName;
+    private String weatherId;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+}
